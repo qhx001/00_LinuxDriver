80b874f29e6f69e95fc01532d6b16b0de25f030f
qihuixin <18712839002@163.com>
2020-08-02 22:44:14 +0800

    device tree of function

---
+++ 005_device_tree_of_function/driver.c
@@ -0,0 +1,137 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/of.h>
+
+
+#if 0
+    backlight {
+        compatible = "pwm-backlight";
+        pwms = <&pwm1 0 1000>;
+        brightness-levels = <0 1 2 3 4 5 6 8 10>;
+        default-brightness-level = <8>;
+        status = "okay";
+    };
+#endif
+
+static int __init driver_init(void)
+{
+    int ret = 0;
+    int i = 0;
+    struct device_node *backlight_node_ptr = NULL;
+    struct property *compatible_ptr = NULL;
+    const char *status_ptr = NULL;
+    unsigned int level = 0;
+    int elem_size = 0;
+    int elem_value = 0;
+
+    /* 1. Get device node. */
+    #if 1
+    /*
+     * Get device node info by path. 
+     * static inline struct device_node *of_find_node_by_path(const char *path)
+     */
+    backlight_node_ptr = of_find_node_by_path("/backlight");
+    #else
+    /*
+     * Get device node info by node name.
+     * struct device_node *of_find_node_by_name(struct device_node *from, const char *name)
+     */
+    backlight_node_ptr = of_find_node_by_name(NULL, "backlight");
+    #endif
+    if (!backlight_node_ptr) {
+        ret = -1;
+        goto err_find_node;
+    }
+
+    /* 2.Get Property info. */
+    /* struct property *of_find_property(const struct device_node *np,
+					                     const char *name,
+					                     int *lenp);
+    */
+    compatible_ptr = of_find_property(backlight_node_ptr, "compatible", NULL);
+    if (!compatible_ptr) {
+        ret = -1;
+        goto err_find_property;
+    }
+    printk("compatile = %s\r\n", (char *)compatible_ptr->value);
+
+    /* extern int of_property_read_string(const struct device_node *np,
+				                          const char *propname,
+				                          const char **out_string);
+    */
+    ret = of_property_read_string(backlight_node_ptr, "status", &status_ptr);
+    if (ret) {
+        ret = -1;
+        goto err_read_string;
+    }
+    printk("status = %s\r\n", status_ptr);
+
+    /* static inline int of_property_read_u32(const struct device_node *np,
+				                              const char *propname,
+				                              u32 *out_value) 
+    */
+    ret = of_property_read_u32(backlight_node_ptr, "default-brightness-level", &level);
+    if (ret) {
+        ret = -1;
+        goto err_read_u32;
+    }
+    printk("default-brightness-level = %d\r\n", level);
+
+
+    /* int of_property_count_elems_of_size(const struct device_node *np,
+				                           const char *propname, int elem_size);
+       int of_property_read_u32_index(const struct device_node *np,
+				                      const char *propname,
+				                      u32 index,
+                                      u32 *out_value);
+    */
+    ret = of_property_count_elems_of_size(backlight_node_ptr, "brightness-levels", sizeof(u32));
+    if (ret <= 0) {
+        ret = -1;
+        goto err_count_elems;
+    }
+    elem_size = ret;
+    printk("elem_size = %d\r\n", elem_size);
+    for (i = 0; i < elem_size; i++) {
+        ret = of_property_read_u32_index(backlight_node_ptr,
+                                        "brightness-levels",
+                                         i, &elem_value);
+        if (ret) {
+            ret = -1;
+            goto err_read_u32_index;
+        }
+        printk("elem_value = %d\r\n", elem_value);
+    }
+
+    printk("%s success!\r\n", __FUNCTION__);
+    return ret;
+
+err_read_u32_index:
+    printk("read u32 index failed!\r\n");
+    return ret;
+err_count_elems:
+    printk("count elems failed!\r\n");
+    return ret;
+err_read_u32:
+    printk("read u32 failed!\r\n");
+    return ret;
+err_read_string:
+    printk("read string failed!\r\n");
+    return ret;
+err_find_property:
+    printk("find property failed!\r\n");
+    return ret;
+err_find_node:
+    printk("find node failed!\r\n");
+    return ret;
+}
+
+static void __exit driver_exit(void)
+{
+    printk("%s success!\r\n", __FUNCTION__);
+}
+
+module_init(driver_init);
+module_exit(driver_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("QHX");
